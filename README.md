387b515e7ac627b8eadf1b334db76cab8e526c0d
rimatik <slaven@rimatik.hr>
2023-12-04 15:57:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,123 @@
+# icp-nft-manager
+
+This project is a canister that leverages the Internet Computer Protocol (ICP) and the Azle framework. It is designed to interact with the CoinGecko API for fetching real-time nft prices and enables users to manage a watchlist of their favorite nfts.
+
+## Key Components
+
+- **Nft**: Records the essential details of a nft, such as its name and symbol.
+- **UserFavorites**: Associates a user's unique Principal identifier with their list of favorite nfts.
+- **userFavoritesStorage**: Utilizes `StableBTreeMap` for stable storage, ensuring persistence of user favorites across system updates.
+
+## Features
+
+- **getNftPrice**: Retrieves the current price of a specified nft in USD from the CoinGecko API.
+- **saveFavoriteNft**: Allows a user to add a nft to their favorites list.
+- **removeFavoriteNft**: Permits a user to remove a nft from their favorites list.
+- **getFavoriteNfts**: Fetches the list of a user's favorite nfts.
+
+## Interaction
+
+Users can interact with the canister via its public methods. The application supports both reading and writing operations, enabling users to update their watchlist and access up-to-date pricing information.
+
+## External HTTP Requests
+
+The canister makes use of `managementCanister.http_request` to execute outbound HTTP requests to the CoinGecko API.
+
+## Purpose
+
+The provided code snippet serves an educational purpose, demonstrating how to build a canister on the ICP using the Azle framework.
+
+## How to run the project
+
+- Clone the repository
+
+```bash
+git clone https://github.com/rimatik/icp-nft-manager.git
+```
+
+- Install dependencies
+
+```bash
+npm install
+```
+
+- Start DFX (More about the flags can be found [here](https://internetcomputer.org/docs/current/references/cli-reference/dfx-start#flags))
+
+```bash
+dfx start --background --clean
+```
+
+- Deploy canister
+
+```bash
+dfx deploy
+```
+
+Once the command completes, you should see a message indicating the successful deployment of your canisters. The output will include URLs for interacting with your backend canister through the Candid interface.
+
+Example:
+
+```bash
+Deployed canisters.
+URLs:
+  Backend canister via Candid interface:
+    nft_manager: http://127.0.0.1:4943/?canisterId=ce2us-64aaa-aaa2a-qabbq-aai&id=bd3sg-teava-aaaaa-qaaba-cai
+```
+
+- Stop DFX when done
+
+```bash
+dfx stop
+```
+
+# Use cases
+
+Use cases can be done through the **Candid interface** or using the command line with the commands below.
+
+## Get coin price by calling CoinGecko API
+
+```bash
+dfx canister call nft_manager getNftPrice '(1)'
+```
+
+You will get a message (will be different because of the nft price):
+
+```rust
+(variant { Ok = "The nft of crypto punk is $100" })
+```
+
+## Add coin to favorites
+
+```bash
+dfx canister call nft_manager saveFavoriteNft '(record {"name"="Crypto punk"; "symbol"="PUNK";})'
+```
+
+You will get a message:
+
+```rust
+(variant { Ok = "Nft Crypto punk added to favorites" })
+```
+
+## Get all favorites
+
+```bash
+dfx canister call nft_manager getFavoriteNfts
+```
+
+You will get a message:
+
+```rust
+(variant { Ok = vec { record { name = "Crypto punk"; symbol = "PUNK" } } })
+```
+
+## Remove coin from favorites
+
+```bash
+dfx canister call nft_manager removeFavoriteNft '(1)'
+```
+
+You will get a message:
+
+```rust
+(variant { Ok = "Nft crypto punk removed from favorites" })
+```
---
+++ src/index.ts
@@ -0,0 +1,149 @@
+import {
+  Canister,
+  Err,
+  None,
+  Ok,
+  Principal,
+  Record,
+  Result,
+  Some,
+  StableBTreeMap,
+  Vec,
+  ic,
+  query,
+  text,
+  update,
+} from "azle";
+
+import {
+  HttpResponse,
+  HttpTransformArgs,
+  managementCanister,
+} from "azle/canisters/management";
+
+// Define a record for a nft with just the name and symbol
+const Nft = Record({
+  name: text,
+  symbol: text,
+});
+
+// Define a record for user's favorite nfts
+const UserFavorites = Record({
+  id: Principal,
+  favorites: Vec(Nft),
+});
+
+// Define a simple storage structure for user favorites
+const userFavoritesStorage = StableBTreeMap(Principal, UserFavorites, 1);
+
+/**
+ * Defines a Canister object with three methods:
+ * - getNft: get current data (name, price_floor, volume_24h ...) for an NFT collection using the CoinGecko API.
+ * - saveFavoriteNft: saves a given Nft as a favorite for the current user.
+ * - removeFavoriteNft: removes Nft as a favorite for the current user.
+ * - getFavoriteNfts: retrieves the list of favorite nfts for the current user.
+ */
+export default Canister({
+  getCoinPrice: update([text], Result(text, text), async (nftId) => {
+    const nft = nftId.toLowerCase();
+
+    const response = await ic.call(managementCanister.http_request, {
+      args: [
+        {
+          url: `https://api.coingecko.com/api/v3/nfts/${nft}`,
+          max_response_bytes: Some(5_000n),
+          method: {
+            get: null,
+          },
+          headers: [],
+          body: None,
+          transform: Some({
+            function: [ic.id(), "transformHttpResponse"] as [Principal, string],
+            context: Uint8Array.from([]),
+          }),
+        },
+      ],
+      cycles: 90_000_000n,
+    });
+
+    if (Number(response.status) !== 200) {
+      return Err("Failed to get coin price");
+    }
+
+    var body = JSON.parse(new TextDecoder().decode(response.body));
+
+    if (!body[nft]) {
+      return Err("Nft not found");
+    }
+
+    const price = body[nft].usd;
+    return Ok(`The price of ${nft} is $${price}`);
+  }),
+
+  saveFavoriteNft: update([Nft], Result(text, text), (nft) => {
+    const userId = ic.caller();
+
+    const entry = {
+      id: userId,
+      favorites: [nft],
+    };
+
+    if (userFavoritesStorage.containsKey(userId) === false) {
+      userFavoritesStorage.insert(userId, entry);
+    } else {
+      const favorites = userFavoritesStorage.get(userId);
+      entry.favorites = [...favorites.Some.favorites, nft];
+      userFavoritesStorage.insert(userId, entry);
+    }
+
+    return Ok(`Nft ${nft.name} added to favorites`);
+  }),
+  removeFavoriteNft: update([text], Result(text, text), (nftSymbol) => {
+    const userId = ic.caller();
+
+    if (userFavoritesStorage.containsKey(userId) === false) {
+      return Err("No favorites found");
+    }
+
+    const nfts = userFavoritesStorage.get(userId).Some.favorites;
+
+    const nft: typeof Nft = nfts.find(
+      (nft: typeof Nft) => nft.symbol === nftSymbol
+    );
+
+    if (!nft) {
+      return Err("Nft not found");
+    }
+
+    const favorites = nfts.filter(
+      (nft: typeof Nft) => nft.symbol !== nftSymbol
+    );
+
+    const entry = {
+      id: userId,
+      favorites: favorites,
+    };
+
+    userFavoritesStorage.insert(userId, entry);
+
+    return Ok(`Nft ${nft.name} removed from favorites`);
+  }),
+
+  getFavoriteNfts: query([], Result(Vec(Nft), text), () => {
+    const userId = ic.caller();
+
+    if (userFavoritesStorage.containsKey(userId) === false) {
+      return Err("No favorites found");
+    }
+
+    const favorites = userFavoritesStorage.get(userId);
+
+    return favorites ? Ok(favorites.Some.favorites) : Err("No favorites found");
+  }),
+  transformHttpResponse: query([HttpTransformArgs], HttpResponse, (args) => {
+    return {
+      ...args.response,
+      headers: [],
+    };
+  }),
+});
